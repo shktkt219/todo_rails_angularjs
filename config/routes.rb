7aa9a3e955d3aa032d63a15d05826fc997523efd
shoko furushita <shktkt219@gmail.com>
2016-11-24 14:12:26 -0500

    edit routes for single page route

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  root 'templates#index'
-  get '/templates/dashboard' => 'templates#dashboard'
-  get '/templates/todo_list' => 'templates#todo_list'
-
   namespace :api, defaults: { format: :json } do
     resources :todo_lists, only: [:index, :show, :create, :destroy] do
       resources :todos, except: [:index, :new, :edit, :show]
     end
   end
+
+  get '/dashboard' => 'templates#index'
+  get '/todo_lists/id' => 'templates#index'
+  get '/templates/:path_html' => 'templates#template', constraints: { path: /.+/ }
+  root 'templates#index'
+
 end
