c37036973d87905881b2d2d8e936dfe6d25ca162
shoko furushita <shktkt219@gmail.com>
2016-11-24 21:15:39 -0500

    add relationp to user model

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -1,4 +1,5 @@
 class TodoList < ActiveRecord::Base
+  belongs_to :user
   has_many :todos, dependent: :destroy
 
   validates :name, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :todo_lists, dependent: :destroy
+  has_many :todos, through: :todo_lists
+
+  validates :name, presence: true
 end
