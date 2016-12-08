01dfd8a682f5b5771391ea9562e3cb3192f57c6a
shoko furushita <shktkt219@gmail.com>
2016-12-08 15:09:58 -0500

    modify app.coffee to app.js

---
+++ app/assets/javascripts/app.js
@@ -0,0 +1,63 @@
+app = angular.module('todoApp', [
+  'ui.bootstrap',
+  'ui.router',
+  'ngMessages']);
+
+// CSRF(Cross-site Request Forgery)token
+app.config(function($httpProvider){
+  var authToken;
+  authToken = $("meta[name=\"csrf-token\"]")
+  .attr("content");
+  return $httpProvider.defaults.headers.common["X-CSRF-TOKEN"] = authToken;
+});
+
+
+app.config(function($stateProvider, $urlRouterProvider, $locationProvider){
+  $urlRouterProvider.otherwise('/home');
+
+  $stateProvider
+
+    .state('home',{
+      url: '/home',
+      templateUrl: '/templates/home.html'
+    })
+
+    .state('index', {
+      url: '/index',
+      templateUrl: '/templates/index.html'
+    })
+
+    .state('help', {
+      url: '/help',
+      templateUrl: '/templates/help.html'
+    })
+
+    .state('about', {
+      url: '/about',
+      templateUrl: '/templates/about.html'
+    })
+
+    .state('index.dashboard', {
+      url: '/dashboard',
+      templateUrl: '/templates/dashboard.html',
+      controller: 'DashboardCtrl as ctrl'
+    })
+
+    .state('index.todolist', {
+      url: '/todo_lists/:list_id',
+      templateUrl: '/templates/todo_list.html',
+      controller: 'TodoListCtrl'
+    })
+
+   $locationProvider.html5Mode(true);
+})
+
+
+// AngularJS can work with turbolinks
+$(document).on('page:load', function(){
+  return $('[ng-app]').each(function(){
+    var module;
+    module = $(this).attr('ng-app');
+    return angular.bootstrap(this, [module]);
+  });
+});
