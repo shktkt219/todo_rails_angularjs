d2b28ce75f71bfcc577d4a58e1f12b4d2e584a31
shoko furushita <shktkt219@gmail.com>
2016-12-11 15:35:09 -0500

    modify TodoListCtrl to get  factory

--- app/assets/javascripts/controllers/TodoListCtrl.js
+++ app/assets/javascripts/controllers/TodoListCtrl.js
@@ -1,51 +1,72 @@
+'use strict';
+
 angular
   .module('todoApp')
-  .controller('TodoListCtrl', function($scope, $stateParams, TodoList, Todo){
+  .controller('TodoListCtrl', ['$scope', '$stateParams', 'TodoListService', 'TodoService', function($scope, $stateParams, TodoListService, TodoService){
 
-    // getAllTodoLists();
-    // getAllTodos();
+    var self = this;
+    var myListId = $stateParams.list_id;
+    self.todo = { id: null, todo_list_id: myListId, description: '', done: false };
 
-    function getAllTodos(){
-      Todo.getAllTodos()
-              .then(function(data){
-                $scope.lists = data;
-              },
-              function(errorResponse){
-                console.error('Error while getting todos');
-              });
+    self.getAllTodos = function(myListId){
+      TodoListService.findTodoList(myListId)
+                     .then(function(data){
+                       self.list = data;
+                     },
+                     function(errorResponse){
+                       console.error('Error while gatting todos');
+                     });
+    };
+
+    self.createTodo = function(myListId, todo){
+      TodoService.createTodo(myListId, todo)
+                 .then(self.getAllTodos(myListId), function(errorResponse){
+                   console.error('Error while creating todo');
+                 });
+    };
+
+    self.updateTodo = function(myListId, todo, id){
+      TodoService.updateTodo(myListId, todo, id)
+                 .then(self.getAllTodos(myListId), function(errorResponse){
+                   console.error('Error while updating todo');
+                 });
     }
 
-    return $scope.list = TodoList.find($stateParams.list_id, function(res){
-      return $scope.totalTodos = res.totalTodos;
-    });
+    self.deleteTodo = function(myListId, id){
+      TodoService.deleteTodo(myListId, id)
+                 .then(self.getAllTodos(myListId), function(errorResponse){
+                   console.error('Error while deleting todo');
+                 });
+    }
 
-    function addTodo(todoDescription){
-      var todo;
-      todo = Todo.createTodo({
-        description: todoDescription,
-        done: false
-      });
-      $scope.list.todos.push(todo);
-      return $scope.todoDescription = "";
+    self.list = self.getAllTodos(myListId);
+
+    self.addTodo = function(){
+      self.createTodo(myListId, self.todo);
+      self.reset();
     };
 
-    function deleteTodo(todo){
-      Todo.deleteTodos(todo);
-      return $scope.list.todos.splice($scope.list.todos.indexOf(todo), 1);
+    self.reset = function(){
+      self.todo = { id: null, todo_list_id: myListId, description: '', done: false };
     };
 
-    function toggleTodo(todo){
-      return Todo.updateTodo(todo, {
+    self.removeTodo = function(id){
+      self.deleteTodo(myListId, id);
+    };
+
+    self.toggleTodo = function(todo, id){
+      self.updateTodo(myListId, self.todo.done = {
         done: todo.done
-      });
+      }, id);
     };
 
-    $scope.filter = {
+    self.filter = {
       done: { done: true },
       remaining: { done: false }
     };
 
-    $scope.changeFilter = function(filter){
-      return $scope.currentFilter = filter;
+    self.changeFilter = function(filter){
+      self.currentFilter = filter;
     };
-  });
+
+  }]);
