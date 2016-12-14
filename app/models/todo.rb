5054b9e9544072113c0092fb468fe13dbdc7878d
shoko furushita <shktkt219@gmail.com>
2016-12-14 00:24:20 -0500

    add validation to todos

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -2,6 +2,6 @@ class Todo < ActiveRecord::Base
   belongs_to :todo_list
 
   validates :todo_list_id, presence: true
-  validates :description, presence: true, length: { maximum: 255 }
+  validates :description, presence: true, length: { maximum: 25 }
   validates :done, inclusion: { in: [true, false]}
 end
